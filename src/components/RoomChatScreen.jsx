bfc855ef03b8a1d697205a05307af70892a8fc91
ngan <vutran@sheridancollege.ca>
2023-12-13 19:21:21 -0500

    Push new msg to local array successfully

--- src/components/RoomChatScreen.jsx
+++ src/components/RoomChatScreen.jsx
@@ -6,11 +6,20 @@ import { getDocs, addDoc } from "firebase/firestore"
 export function RoomChatScreen(props) {
     const [currentMsg, setCurrentMsg] = useState("")
     const [msgHistory, setMsgHistory] = useState(props.roomMsg)
+    console.log(msgHistory)
+    
+    function pushNewMsg(onSubmitEventInfo) {
+        onSubmitEventInfo.preventDefault()
+        setMsgHistory(prev => [...prev, {
+            userId: 333,
+            message: currentMsg
+        }])
+    }
 
     return (
         <div className="room-chat-box">
             <h1>Room ID: {props.roomId}</h1>
-            <form className="message-input-box">
+            <form className="message-input-box" onSubmit={e => pushNewMsg(e)}>
                 <input type="text" name="message" id="message" onChange={e => setCurrentMsg(e.target.value)} />
                 <button>Send</button>
             </form>

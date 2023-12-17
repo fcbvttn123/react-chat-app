78611903c62fd0c2eb1987ea091eda0157b72c78
ngan <vutran@sheridancollege.ca>
2023-12-16 20:54:16 -0500

    Features Done: get all messages and print available messages

--- src/components/RoomChatScreen.jsx
+++ src/components/RoomChatScreen.jsx
@@ -1,9 +1,29 @@
 import { useState, useEffect } from "react"
 
 import { roomsFireStoreCollection, db } from "../firebase"
-import { setDoc, doc } from "firebase/firestore"
+import { setDoc, doc, snapshotEqual } from "firebase/firestore"
+
+import { v4 } from "uuid"
+
+// Realtime DB
+import { realtimeDb } from "../firebase"
+import {ref, onValue, set} from "firebase/database"
 
 export function RoomChatScreen(props) {
+
+    const [allMessages, setAllMessages] = useState(null)
+
+    useEffect(() => {
+        let reference = ref(realtimeDb, `rooms/${props.roomId}/messages`)
+        onValue(reference, snapShot => {
+            let msgArr = []
+            snapShot.forEach(child => {
+                msgArr.push(child.val())
+            })
+            setAllMessages(msgArr)
+        })
+    }, [])
+    
     return (
         <div className="room-chat-box">
             <h1>Room ID: {props.roomId}</h1>
@@ -11,7 +31,9 @@ export function RoomChatScreen(props) {
                 <input type="text" name="message" id="message" />
                 <button>Send</button>
             </form>
-            <div className="display-msg"></div>
+            <div className="display-msg">
+                {allMessages && allMessages.map(e => <p key={v4()}>{e.userId}: {e.message}</p>)}
+            </div>
         </div>
     )
 }
